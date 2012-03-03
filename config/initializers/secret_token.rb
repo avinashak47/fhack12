c4d040cfdfed73f65a552c0cde4e15e8ae76ecf7
seung1 <d4park@ucsd.edu>
2012-03-02 20:11:28 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fhack::Application.config.secret_token = '8c820d84832306e83db222414ed922adfde65d2de463a455d9c487f4bd9d54a7a6d2d5a257ea6257a1233b011731bed2c925bc209b11ad6154de8a5220bba1b2'

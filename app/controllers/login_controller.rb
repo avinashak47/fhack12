356d57bd2aa706ef8d6e4530a3ab3eecd0b9849a
Avinash Ananth <avi@localhost.localdomain>
2012-03-03 08:34:30 -0800

    basic test for vering json response of friends data - try 5, more oauth stuff

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,14 +2,15 @@ class LoginController < ApplicationController
 
 def fb_login
 	oauth = Koala::Facebook::OAuth.new(FB[:id], FB[:secret], 'http://gentle-fire-7931.heroku.com/FB/FBCallback')
-	render :text=>@oauth.url_for_oauth_code(:permissions=>"user_actions.music,user_likes,friends_likes,user_actions.music")
+	render :text=>oauth.url_for_oauth_code(:permissions=>"user_actions.music,user_likes,friends_likes,user_actions.music")
 end
 
 def fb_callback
 	 if(params['code'].nil?)
 		render :json=>{"Error"=>"Put more shiz in from the error foo!"}
 	 else
-	 	oauth_token = params['code']
+		oauth = Koala::Facebook::OAuth.new(FB[:id], FB[:secret], 'http://gentle-fire-7931.heroku.com/FB/FBCallback')
+	 	oauth_token = oauth.get_access_token(params['code'])
 		DataController::build_song_index(oauth_token)	
 	 end
 end
